2e31b6689a8fb28712e637789e7364af860b1298
Rintaro-Suzuki <damdam_0616@icloud.com>
2025-03-06 18:07:55 +0900

    Feature/revise linpred (#1)
    * 線形回帰を修正
    
    * 線形予測修正2回目

--- linear_pred.py
+++ linear_pred.py
@@ -20,37 +20,53 @@ class CustomAgent(base.Agent):
         for i in range(past_years):
             years[i] = i
             for agent in agents:
-                if len(agent.history_oil) == 0 or agent.history_oil == 1:
+                if len(agent.history_oil) == 0 or len(agent.history_oil) == 1:
                     lack_of_history = 1
                     continue
                 elif agent.ind == self.ind:
                     continue
                 elif len(agent.history_oil) < past_years-i:
                     continue
-                # 過去の重油船とグリーン船の合計を集計
+                # 過去の重油船とグリーン船の差分の合計を集計
                 past_sum_n_oils[i] += agent.history_oil[-(past_years-i)]
                 past_sum_n_greens[i] += agent.history_green[-(past_years-i)]
+            if not i:
+                continue
+                # 差分を一番過去から足し合わせる
+            for j in range(i):
+                past_sum_n_oils[i] += past_sum_n_oils[j]
+                past_sum_n_greens[i] += past_sum_n_greens[j]
 
         # 線形回帰
-        a_oil, b_oil = np.polyfit(years,past_sum_n_oils,1)
-        a_green, b_green = np.polyfit(years, past_sum_n_greens,1)
+        a_oil, _ = np.polyfit(years,past_sum_n_oils,1)
+        a_green, _ = np.polyfit(years, past_sum_n_greens,1)
+        print("green燃料船の差分の和")
+        print(past_sum_n_greens)
+        print("greenの傾き")
+        print(a_green)
+
 
         pred_n_oil = np.zeros(future_years)
         pred_n_green = np.zeros(future_years)
 
         for j in range(future_years):
+            for agent in agents:
+                if agent.ind == self.ind:
+                    continue
+                pred_n_oil[j] += agent.n_oil
+                pred_n_green[j] += agent.n_green
+
             if lack_of_history:
                 if j == 0:
-                    pred_n_oil[j] = random.randint(-5,5)*len(agents)+env.total_n_oil*3/4
-                    pred_n_green[j] = random.randint(-5,5)*len(agents)+env.total_n_green*3/4
+                    pred_n_oil[j] = random.randint(-5,5)*len(agents)
+                    pred_n_green[j] = random.randint(-5,5)*len(agents)
                 else:
                     pred_n_oil[j] = pred_n_oil[0]
                     pred_n_green[j] = pred_n_green[0]
             else:
-                pred_n_oil[j] = pred_n_oil[j-1] + a_oil*(j+past_years) + b_oil
-                pred_n_green[j] = pred_n_green[j-1] + a_green*(j+past_years) + b_green
-        pred_n_oil += env.total_n_oil*3/4
-        pred_n_green += env.total_n_green*3/4
+                pred_n_oil[j] += a_oil*(j+1)
+                pred_n_green[j] += a_green*(j+1)
+
         pred_n_oil = np.maximum(pred_n_oil,0)
         pred_n_green = np.maximum(pred_n_green,0)
 
